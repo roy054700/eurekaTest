06eeffe4d25eddac5ffd5c3bf318af27bd733dd2
Your Name <you@example.com>
2019-12-02 10:12:52 +0800

    第一次提交

--- roy-user/src/main/java/com/example/UserApplication.java
+++ roy-user/src/main/java/com/example/UserApplication.java
@@ -2,9 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
-public class UserApplication {
+@EnableDiscoveryClient
+@EnableFeignClients
+public class UserApplication<EnableFeignClients> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
